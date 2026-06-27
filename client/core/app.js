0611e2b6bf1978717245868b1c9b5cb286868aa7
bkkskalan-vttdrgbn <bkk.skalanbdl@gmail.com>
2026-06-27 10:17:14 +0700

    Update app.js

--- client/core/app.js
+++ client/core/app.js
@@ -1,1 +1,20 @@
+/**
+ * ==========================================================
+ * DragonBoard Application
+ * ==========================================================
+ */
 
+import { Config } from "./config.js";
+import { GameState } from "./state.js";
+
+function initializeApplication() {
+
+    console.log(`${Config.APP_NAME} ${Config.VERSION}`);
+
+    GameState.application.ready = true;
+
+    console.log("Application Ready");
+
+}
+
+export { initializeApplication };
