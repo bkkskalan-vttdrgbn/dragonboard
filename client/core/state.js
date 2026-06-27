fd9a5308adf2e27218eb244f73b8d91b71a3cd73
bkkskalan-vttdrgbn <bkk.skalanbdl@gmail.com>
2026-06-27 10:16:28 +0700

    Update state.js

--- client/core/state.js
+++ client/core/state.js
@@ -1,1 +1,41 @@
+/**
+ * ==========================================================
+ * DragonBoard Runtime State
+ * ==========================================================
+ * Stores the current application state.
+ * Never store DOM elements here.
+ */
 
+export const GameState = {
+
+    application: {
+
+        ready: false,
+
+        currentScreen: "splash"
+
+    },
+
+    runtime: {
+
+        campaign: null,
+
+        currentMap: null,
+
+        selectedToken: null,
+
+        round: 0
+
+    },
+
+    camera: {
+
+        zoom: 1,
+
+        x: 0,
+
+        y: 0
+
+    }
+
+};
